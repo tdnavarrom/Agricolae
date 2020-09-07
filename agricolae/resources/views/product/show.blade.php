179953cb3cd1a181d4dec0bb156ec1c1532ab5b9
Tomas David Navarro Munera <tdnavarrom@eafit.com>
2020-09-07 10:29:56 -0500

    Product View 2.0

--- agricolae/resources/views/product/show.blade.php
+++ agricolae/resources/views/product/show.blade.php
@@ -8,8 +8,8 @@
 
 <br>
 
-<div class="container">
-    <div class="row justify-content-center">
+<div class="container col-md-10 justify-content-center">
+    <div class="row">
         <div class="col-md-8">
             <div class="card">
                 <h1 class="title_name">{{ $data["product"]["name"] }} </h1>
@@ -26,23 +26,26 @@
                 <h5>{{ $data["product"]["description"] }}</h5>
                 <p style='padding-top:2%;'><button>@lang('messages.add_cart')</button></p>
             </div>
-
+        </div>
+        <div class="col-md-4">
             <h1 class="title_name"'>Reviews</h1>
 
-            <div class="container">
+            <div class="container mt-2 mb-2">
                 @foreach($data['product']->reviews as $review)
-                    <div class="card">
+                    <div class="card mt-2 mb-2">
                         <div class="card-header">
-                            <b class='subtitle score'>Score: {{ $review->score }}</b>
-                            <h1 style='display:inline'>{{ $review->title }}</h1>
+                            <h2 style='display:inline'>{{ $review->title }}</h2>
                         </div>
                         <div class="card-body">
+                            <b class='subltitle'>@lang('messages.review_score'): {{ $review->score }}</b>
                             <p>{{ $review->description }}</p>
                         </div>
                     </div>
-                    <br>
-                @endforeach                
+                @endforeach
 
+                <div class="mb-2">
+                    <a href="{{ route('review.create', $data['product']->id) }}"> <button id='create_review'>@lang('messages.review_create')</button> </a>
+                </div>          
             </div>
         </div>
     </div>
