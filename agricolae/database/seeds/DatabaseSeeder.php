442a77a5b1debd0d936197e3e7e1d59df2dd379b
Tomas Navarro <tomas-navarro@hotmail.com>
2020-10-05 22:38:56 -0500

    Created Factory

--- agricolae/database/seeds/DatabaseSeeder.php
+++ agricolae/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        App\User::create([
+            'name' => 'Tomas',
+            'last_name' => 'Navarro',
+            'cell_phone' => '3005529391',
+            'email' => 'tomas-navarro@hotmail.com',
+            'user_type' => 'farmer',
+            'password' => bcrypt("TomasDavid2001"), // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        factory(App\User::class, 20)->create();
+        factory(App\Product::class, 60)->create();
+        factory(App\Review::class, 120)->create();
     }
 }
