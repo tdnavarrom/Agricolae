810dfa57b579b3f5934631683a1e846bf05bca19
Tomas Navarro <tomas-navarro@hotmail.com>
2020-10-08 22:53:49 -0500

    Finished api v1

--- agricolae/app/Http/Controllers/Api/ProductController.php
+++ agricolae/app/Http/Controllers/Api/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */ 
     public function index()
     {
-        //
+        return response()->json(Product::paginate());
     }
 
     /**
@@ -58,7 +58,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return response()->json($product, 200);
     }
 
     /**
@@ -70,7 +70,10 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
+
+        return response()->json($product, 200);
+
     }
 
     /**
@@ -81,6 +84,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response()->json(null, 204);
     }
 }
